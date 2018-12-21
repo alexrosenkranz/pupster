53eaf4a408661c5aaf6f57595407351a457d5ab5
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2018-12-20 19:19:28 -0500

    Updates

---
+++ precache-manifest.217a8045ed5431ceaaaea889abe0c6c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aebd113796fc731b1e614c000517652b",
+    "url": "/pupster/static/media/doge.aebd1137.jpg"
+  },
+  {
+    "revision": "5e0ceb9ce92ccaf83191",
+    "url": "/pupster/static/js/runtime~main.5e0ceb9c.js"
+  },
+  {
+    "revision": "dce75d3d124a79354340",
+    "url": "/pupster/static/js/main.dce75d3d.chunk.js"
+  },
+  {
+    "revision": "0b41cd2e256a755c231c",
+    "url": "/pupster/static/js/1.0b41cd2e.chunk.js"
+  },
+  {
+    "revision": "0ca67b94424e3aad8311ecf79451fe39",
+    "url": "/pupster/index.html"
+  }
+];
